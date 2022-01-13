c1b53893149a58fd7ecc97d2f8d227a78795922b
nimoc <nimocc@126.com>
2022-01-13 23:38:36 +0800

    RawStdDecode =>DecodeRawStd

--- base64.go
+++ base64.go
@@ -2,25 +2,25 @@ package xbase64
 
 import "encoding/base64"
 
-func RawStdEncode (src []byte) (base64Bytes []byte) {
+func EncodeRawStd (src []byte) (base64Bytes []byte) {
 	enc := base64.RawStdEncoding
 	base64Bytes = make([]byte, enc.EncodedLen(len(src)))
 	enc.Encode(base64Bytes, src)
 	return base64Bytes
 }
-func RawStdDecode(src []byte) (base64Bytes []byte, err error) {
+func DecodeRawStd(src []byte) (base64Bytes []byte, err error) {
 	enc := base64.RawStdEncoding
 	dbuf := make([]byte, enc.DecodedLen(len(src)))
 	n, err := enc.Decode(dbuf, src)
 	return dbuf[:n], err
 }
-func RawURLEncode (src []byte) (base64Bytes []byte) {
+func EncodeRawURL (src []byte) (base64Bytes []byte) {
 	enc := base64.RawURLEncoding
 	base64Bytes = make([]byte, enc.EncodedLen(len(src)))
 	enc.Encode(base64Bytes, src)
 	return base64Bytes
 }
-func RawURLDecode(src []byte) (base64Bytes []byte, err error) {
+func DecodeRawURL(src []byte) (base64Bytes []byte, err error) {
 	enc := base64.RawURLEncoding
 	dbuf := make([]byte, enc.DecodedLen(len(src)))
 	n, err := enc.Decode(dbuf, src)
--- base64_test.go
+++ base64_test.go
@@ -6,20 +6,20 @@ import (
 )
 
 func TestRawBase64StdEncode(t *testing.T) {
-	base64Bytes := RawStdEncode([]byte("离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。"))
+	base64Bytes := EncodeRawStd([]byte("离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。"))
 	assert.Equal(t,base64Bytes, []byte("56a756a75Y6f5LiK6I2J77yM5LiA5bKB5LiA5p6v6I2j44CC6YeO54Gr54On5LiN5bC977yM5pil6aOO5ZC55Y+I55Sf44CC6L+c6Iqz5L615Y+k6YGT77yM5pm057+g5o6l6I2S5Z+O44CC5Y+I6YCB546L5a2Z5Y6777yM6JCL6JCL5ruh5Yir5oOF44CC"))
 }
 func TestRawBase64StdDecode(t *testing.T) {
-	base64Bytes, err := RawStdDecode([]byte("56a756a75Y6f5LiK6I2J77yM5LiA5bKB5LiA5p6v6I2j44CC6YeO54Gr54On5LiN5bC977yM5pil6aOO5ZC55Y+I55Sf44CC6L+c6Iqz5L615Y+k6YGT77yM5pm057+g5o6l6I2S5Z+O44CC5Y+I6YCB546L5a2Z5Y6777yM6JCL6JCL5ruh5Yir5oOF44CC"))
+	base64Bytes, err := DecodeRawStd([]byte("56a756a75Y6f5LiK6I2J77yM5LiA5bKB5LiA5p6v6I2j44CC6YeO54Gr54On5LiN5bC977yM5pil6aOO5ZC55Y+I55Sf44CC6L+c6Iqz5L615Y+k6YGT77yM5pm057+g5o6l6I2S5Z+O44CC5Y+I6YCB546L5a2Z5Y6777yM6JCL6JCL5ruh5Yir5oOF44CC"))
 	assert.NoError(t, err)
 	assert.Equal(t, base64Bytes, []byte("离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。"))
 }
 func TestRawBase64URLEncode(t *testing.T) {
-	base64Bytes := RawURLEncode([]byte("离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。"))
+	base64Bytes := EncodeRawURL([]byte("离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。"))
 	assert.Equal(t,base64Bytes, []byte("56a756a75Y6f5LiK6I2J77yM5LiA5bKB5LiA5p6v6I2j44CC6YeO54Gr54On5LiN5bC977yM5pil6aOO5ZC55Y-I55Sf44CC6L-c6Iqz5L615Y-k6YGT77yM5pm057-g5o6l6I2S5Z-O44CC5Y-I6YCB546L5a2Z5Y6777yM6JCL6JCL5ruh5Yir5oOF44CC"))
 }
 func TestRawBase64URLDecode(t *testing.T) {
-	base64Bytes, err := RawURLDecode([]byte("56a756a75Y6f5LiK6I2J77yM5LiA5bKB5LiA5p6v6I2j44CC6YeO54Gr54On5LiN5bC977yM5pil6aOO5ZC55Y-I55Sf44CC6L-c6Iqz5L615Y-k6YGT77yM5pm057-g5o6l6I2S5Z-O44CC5Y-I6YCB546L5a2Z5Y6777yM6JCL6JCL5ruh5Yir5oOF44CC"))
+	base64Bytes, err := DecodeRawURL([]byte("56a756a75Y6f5LiK6I2J77yM5LiA5bKB5LiA5p6v6I2j44CC6YeO54Gr54On5LiN5bC977yM5pil6aOO5ZC55Y-I55Sf44CC6L-c6Iqz5L615Y-k6YGT77yM5pm057-g5o6l6I2S5Z-O44CC5Y-I6YCB546L5a2Z5Y6777yM6JCL6JCL5ruh5Yir5oOF44CC"))
 	assert.NoError(t, err)
 	assert.Equal(t, base64Bytes, []byte("离离原上草，一岁一枯荣。野火烧不尽，春风吹又生。远芳侵古道，晴翠接荒城。又送王孙去，萋萋满别情。"))
 }
