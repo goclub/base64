bb4c5b37168e2c97df443bbaaf5f0e7146cc85e9
nimoc <nimocc@126.com>
2022-04-11 10:43:25 +0800

    func sign

--- base64.go
+++ base64.go
@@ -2,27 +2,27 @@ package xbase64
 
 import "encoding/base64"
 
-func EncodeRawStd (src []byte) (base64Bytes []byte) {
+func EncodeRawStd(source []byte) (base64Bytes []byte) {
 	enc := base64.RawStdEncoding
-	base64Bytes = make([]byte, enc.EncodedLen(len(src)))
-	enc.Encode(base64Bytes, src)
+	base64Bytes = make([]byte, enc.EncodedLen(len(source)))
+	enc.Encode(base64Bytes, source)
 	return base64Bytes
 }
-func DecodeRawStd(src []byte) (base64Bytes []byte, err error) {
+func DecodeRawStd(base64Bytes []byte) (source []byte, err error) {
 	enc := base64.RawStdEncoding
-	dbuf := make([]byte, enc.DecodedLen(len(src)))
-	n, err := enc.Decode(dbuf, src)
+	dbuf := make([]byte, enc.DecodedLen(len(base64Bytes)))
+	n, err := enc.Decode(dbuf, base64Bytes)
 	return dbuf[:n], err
 }
-func EncodeRawURL (src []byte) (base64Bytes []byte) {
+func EncodeRawURL(src []byte) (base64Bytes []byte) {
 	enc := base64.RawURLEncoding
 	base64Bytes = make([]byte, enc.EncodedLen(len(src)))
 	enc.Encode(base64Bytes, src)
 	return base64Bytes
 }
-func DecodeRawURL(src []byte) (base64Bytes []byte, err error) {
+func DecodeRawURL(base64Bytes []byte) (source []byte, err error) {
 	enc := base64.RawURLEncoding
-	dbuf := make([]byte, enc.DecodedLen(len(src)))
-	n, err := enc.Decode(dbuf, src)
+	dbuf := make([]byte, enc.DecodedLen(len(base64Bytes)))
+	n, err := enc.Decode(dbuf, base64Bytes)
 	return dbuf[:n], err
 }
